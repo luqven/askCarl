3ad17e717f91e9a8937ec55bd120ea596c2a284c
Luis Ball <luqven@gmail.com>
2019-02-10 13:22:08 -0500

    add plan for future animation features

--- src/animations/squash.js
+++ src/animations/squash.js
@@ -10,17 +10,31 @@ class Squash {
     this.pos          = this.token.position;   // [xPos, yPos]
     this.dimensions   = this.token.dimensions; // [w, h] 
     this.height       = this.token.dimensions[1];
-    this.deltaY       = 1;   // initial vertical speed
-    this.deltaX       = 0;   // initial horizontal speed
+    this.initialH     = this.token.dimensions[1];
+    this.width        = this.dimensions[0]; // this width gets changed
+    this.initialW     = this.dimensions[0]; // store for later refrence
+    this.deltaY       = 2;   // initial vertical change
+    this.deltaX       = 0;   // initial vertical change
+    this.deltaD       = 10;  // initial dimensions change
     this.acceleration = 20;  // initial accel
-    this.threshhold   = .05; //  stop animationwhen speed reaches this
     this.hitCount     = 1;   // start with 0 bounces
-
+    
     this.bounce        = this.bounce.bind(this);
     this.hitWall       = this.hitWall.bind(this);
     this.increaseAccel = this.increaseAccel.bind(this);
     this.moveInDyDir   = this.moveInDyDir.bind(this);
     this.reverseDeltaY = this.reverseDeltaY.bind(this);
+
+    //////////////////////////////////////
+    // TO DO: threshold params
+    //////////////////////////////////////
+
+    // this.threshold    = this.botWall -50; // pos of squish region of canvas
+    // this.resize       = false;
+    // this.hitThreshold  = this.hitThreshold.bind(this);
+    // this.reverseDeltaD = this.reverseDeltaD.bind(this);
+    // this.changeDimensions = this.changeDimensions.bind(this);
+
   }
 
   hitWall(){
@@ -32,6 +46,33 @@ class Squash {
     }
     return false;
   }
+
+/////////////////////////////////////////
+//    TO DO: Implement dim resizing
+/////////////////////////////////////////
+
+  // hitThreshold() {
+  //   if (this.pos[1] === this.threshold) {
+  //     this.reverseDeltaD();
+  //     this.resize = !this.resize;
+  //     return true;
+  //   } else if (this.pos[1] + this.height === this.botWall) {
+  //     this.reverseDeltaD();
+  //     return true;
+  //   }
+  //   return false;
+  // }
+
+  // changeDimensions() {
+  //   this.width += this.deltaD;
+  //   // this.height += this.deltaD;
+  //   this.token.setDimensions(this.width, this.height)
+  // }
+
+  // reverseDeltaD(){
+  //   // reverse horizontal delta direction
+  //   this.deltaD = this.deltaD * -1;
+  // }
 
   reverseDeltaY(){
     // reverse verticle delta direction
@@ -45,31 +86,22 @@ class Squash {
   moveInDyDir() {
     // move token to postion after adding delta
     this.token.changePosition([this.deltaX, this.deltaY])
-
   }
-
-  increaseDelta() {
-    // increase verticle speed
-    this.deltaY += this.deltaY;
-  }
-
+  
   bounce() {
     this.token.render()
     // move the token in dY direciton
     this.moveInDyDir();
     // check to see if wall was hit
     if (this.hitWall() === true) {
-      console.log('wall hit!');
       // if hit reverse verticle delta polarity
       this.reverseDeltaY();
-      console.log(`deltaY inverted`)
     } else {
       this.increaseAccel();
     }
   }
 
   render(){
-    console.log(this.token);
     setInterval(this.bounce, 50);
   }
 }
