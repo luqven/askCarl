6567e3681d63a8f3262c64051fc0896ee15e177b
Luis Ball <luqven@gmail.com>
2019-02-12 16:35:55 -0500

    change color scheme

--- src/shapes.js
+++ src/shapes.js
@@ -11,7 +11,7 @@ const Shapes = (canvas, animationType) =>  {
 
   const squareDefaults = {
     shape: shapes[0], // square
-    color: colors[Util.getRandom(0, 3)],
+    color: colors[0],
     dimensions: [tokenWidth, tokenWidth, tokenWidth / 2], // [width, height, radius]
     position: [0, canvas.height - tokenWidth],  // [xStartPos, yStartPos]
     walls: [0, canvas.height, 0, canvas.width],  // [topW, botW, leftW, rightW]
@@ -23,7 +23,7 @@ const Shapes = (canvas, animationType) =>  {
 
   const circleDefaults = {
     shape: shapes[1], // circle
-    color: colors[Util.getRandom(0, 3)],
+    color: colors[1],
     dimensions: [tokenWidth + 50, tokenWidth, tokenWidth / 2], // [width, height, radius]
     position: [(canvas.width / 2.0), canvas.height / 3.0],  // [xStartPos - 1/2 * width, yStartPos - 1/2 * height]
     walls: [0, canvas.height, tokenWidth, canvas.width],  // [topW, botW, leftW, rightW]
