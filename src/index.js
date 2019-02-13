bccbda926ce86933355946bec3d7ef180591564e
Luis Ball <luqven@gmail.com>
2019-02-13 12:15:54 -0500

    clean up comments

--- src/index.js
+++ src/index.js
@@ -21,9 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
   })();
   console.log("DOM fully loaded and parsed");
 
-  // const animation1Container = document.getElementById('a1');
-  // const animation2Container = document.getElementById('a2');
-
   const canvas2 = document.getElementById('canvas2');
   const canvas3 = document.getElementById('canvas3');
 
@@ -38,12 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
   cardEvents.mouseEnter();
   cardEvents.mouseLeave();
 
-
-  var animations = [];
-
   // set shapes to render
-  // let squash = Shapes(canvas3, 'bounce').squash;
-
   let square = new CanvasElement({
     target: canvas2,
     location: [270, 800],
@@ -58,16 +50,13 @@ document.addEventListener("DOMContentLoaded", () => {
   })
 
   // store shapes in animations array
+  var animations = [];
   animations.push(square);
   animations.push(squash);
   
   // render the shapes
-  // console.log(square);
   function draw() {
-    // console.log(animations);
     window.requestAnimationFrame(draw)
-    // canvas2.getContext('2d').clearRect(0, 0, canvas2.width, canvas2.height);
-    // canvas3.getContext('2d').clearRect(0, 0, canvas3.width, canvas3.height);
 
     if (square.scrolledToElement() ){
       square.addClasses();
@@ -79,26 +68,6 @@ document.addEventListener("DOMContentLoaded", () => {
       square.reset();
     }
 
-    // // add hover efect when scrolled to specific animation
-    // if (window.pageYOffset > 270 && window.pageYOffset < 800) {
-    //   squash = Shapes(canvas3, 'bounce').squash; // reset squash animation
-    //   // animation1Container.classList.add('hovered')
-    //   animation2Container.classList = ('hover-card-container')
-    //   // square.render();
-    // } 
-    // if (window.pageYOffset > 820) {
-    //   // square = Shapes(canvas2, 'bounce').square; // reset square animation
-    //   animation2Container.classList.add('hovered')
-    //   squash.render();
-    //   // animation1Container.classList = ("hover-card-container card-face frontface2")
-    // }
-
-    // if (window.pageYOffset < 169) {
-    //   animation2Container.classList = ('hover-card-container')
-    //   // animation1Container.classList = ("hover-card-container card-face frontface2")
-    //   squash = Shapes(canvas3, 'bounce').squash; // reset squash animation
-    //   // square = Shapes(canvas2, 'bounce').square; // reset square animation
-    // }
   };
   window.requestAnimationFrame(draw);
 });
