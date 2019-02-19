a9bf95b333f8afd93008412673b395e883d8662d
Luis H. Bajj Jr <luqven@gmail.com>
2019-02-19 18:39:07 -0500

    refactor wall hit detection to use midpoint instead of wall coords

--- src/animations/engine.js
+++ src/animations/engine.js
@@ -36,6 +36,7 @@ class movingObject {
     this.hitWall = this.hitWall.bind(this);
     this.hitHorizWall  = this.hitHorizWall.bind(this);
     this.hitVertWall   = this.hitVertWall.bind(this);
+    this.distanceBetween  = this.distanceBetween.bind(this);
     
   }
   
@@ -43,77 +44,50 @@ class movingObject {
 // wall collision logic
 ////////////////////////////////////////
   hitWall() {
+    if (this.token.type != "circle") {
+      //  get coordinates of the center of the shape
+      this.midPoint = [this.pos[0] + this.width / 2, this.pos[1] + this.height / 2];
+    } else {
+      this.midPoint = this.pos;
+    };
+    // gew the coordinates of shapes 4 corners
     let startX = this.pos[0];
     let startY = this.pos[1];
-
     let topLeft  = this.pos;
     let topRight = [startX + this.width, startY];
     let botLeft  = [startX, startY + this.height];
-    let botRight = [startX + this.width , startY + this.height];
-    // wallIndexes = [1, 3]  // [top / bottm, left / right]
+    let botRight = [startX + this.width , startY + this.height]; 
+    // wallIndexes = [1, 3]  // [top / bottom, left / right]
     let hitWalls = [];
-    // check all four corners against each wall
+    // for each wall
+      // if midP +/- radius <=> wall -> wall hit
+      if( this.midPoint[0] - this.radius <= this.leftWAll[0] ||
+          this.midPoint[0] + this.radius >= this.rightWall[0] ) {
+            hitWalls.push(2);
+      }
+      if (this.midPoint[1] - this.radius <= this.topWall[1] ||
+          this.midPoint[1] + this.radius >= this.botWall[1]) {
+        hitWalls.push(0);
+      }
 
-    if (this.hitHorizWall(topLeft, topRight, botLeft, botRight)) {
-       hitWalls.push(1);
-    };
-    if (this.hitVertWall(topLeft, topRight, botLeft, botRight)) {
-      hitWalls.push(3);
-    };
+      // TODO: object collision detection
+      // if (this.distanceBetween(this.midPoint, otherMidP) <= 0 ){
+      //   debugger
+      //     hitWalls.push(i);
+      // }
     return hitWalls;
   }
 
-  hitHorizWall(topLeft, topRight, botLeft, botRight) {
-      if  (
-      topLeft[0]  <=  this.topWall[0] ||
-      topRight[0] <=  this.topWall[0] ||
-      botLeft[0]  <=  this.topWall[0] ||
-      botRight[0] <=  this.topWall[0] ||  // topWall hit check
-      topLeft[1]  <=  this.topWall[1] ||
-      topRight[1] <=  this.topWall[1] ||
-      botLeft[1]  <=  this.topWall[1] ||
-      botRight[1] <=  this.topWall[1]
-    ) { 
-      return true;
-      } else if (
-      topLeft[0]  <=  this.botWall[0] ||
-      topRight[0] <=  this.botWall[0] ||
-      botLeft[0]  <=  this.botWall[0] ||
-      botRight[0] <=  this.botWall[0] ||  // botWall hit check
-      topLeft[1]  >=  this.botWall[1] ||
-      topRight[1] >=  this.botWall[1] ||
-      botLeft[1]  >=  this.botWall[1] ||
-      botRight[1] >=  this.botWall[1]
-    ) {
-      return true;
-      };
-  };
-  
-  hitVertWall(topLeft, topRight, botLeft, botRight) {
-    if (
-      topLeft[0]  <= this.leftWAll[0] ||
-      topRight[0] <= this.leftWAll[0] ||
-      botLeft[0]  <= this.leftWAll[0] ||
-      botRight[0] <= this.leftWAll[0] ||  // leftWall hit check
-      topLeft[1]  <= this.leftWAll[1] ||
-      topRight[1] <= this.leftWAll[1] ||
-      botLeft[1]  <= this.leftWAll[1] ||
-      botRight[1] <= this.leftWAll[1]
-    ) {
-      return true;
-      } else if (
-      topLeft[0]  >= this.rightWall[0] ||
-      topRight[0] >= this.rightWall[0] ||
-      botLeft[0]  >= this.rightWall[0] ||
-      botRight[0] >= this.rightWall[0] || // right hit check
-      topLeft[1]  <= this.rightWall[1] ||
-      topRight[1] <= this.rightWall[1] ||
-      botLeft[1]  <= this.rightWall[1] ||
-      botRight[1] <= this.rightWall[1]
-    ) { 
-      return true;
-      };
-  };
+  distanceBetween(pos1, pos2) {
+    debugger
+    let xDiff = Math.round(pos2[0]) - Math.round(pos1[0]);
+    let yDiff = Math.round(pos2[1]) - Math.round(pos1[1]);
+    // a**2 + b**2 === c**2
+    let distance = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2))
+    debugger
+    return distance;
+  }
+
 
 ////////////////////////////////////////
 // token delta change logic
@@ -124,12 +98,12 @@ class movingObject {
   }
 
   reverseDeltaY(){
-    // reverse verticle delta direction
+    // reverse vertical delta direction
     this.deltaY =  this.friction * this.deltaY * -1;
   }
 
   reverseDeltaX(){
-    // reverse verticle delta direction
+    // reverse vertical delta direction
     this.deltaX =  this.friction * this.deltaX * -1;
   }
 
@@ -140,7 +114,7 @@ class movingObject {
   }
 
   moveInDyDir() {
-    // move token to postion after adding delta
+    // move token to position after adding delta
     this.token.changePosition([this.deltaX, this.deltaY])
   }
 
@@ -152,31 +126,32 @@ class movingObject {
 
     // change deltaX when between start pos + 10 and  start pos + 30
     // this simulates slow in and slow out animation behavior
-    if (Math.abs(this.pos[0] - this.initialPos[0]) > 10 || Math.abs(this.pos[0] - this.initialPos[0]) < 30) {
+    if (Math.abs(this.pos[0] - this.initialPos[0]) > 10 ||
+        Math.abs(this.pos[0] - this.initialPos[0]) < 30) {
       this.deltaX = this.deltaX * (this.friction / .92)
     }
 
-    // set end condition to true if delats have reached thresholds
-    if (Math.abs(this.deltaY ) < this.thresholdY  && Math.abs(this.deltaX) <= this.thresholdX) {
-      //  
+    // set end condition to true if deltas have reached thresholds
+    if (Math.abs(this.deltaY ) < this.thresholdY &&
+        Math.abs(this.deltaX) <= this.thresholdX) {
       this.over = true
     };
 
     // check if render should end
     // if (this.over) { return null}
 
-    // move the token in dY direciton
+    // move the token in dY direction
     this.moveInDyDir();
 
     // check to see if wall was hit
     const hitWall = this.hitWall();
     // 
     // if hit bottom or top wall
-    if (hitWall.includes(1)) {
+    if (hitWall.includes(0)) {
       this.reverseDeltaY();
     }
     // if hit left or right wall
-    if (hitWall.includes(3)) {
+    if (hitWall.includes(2)) {
       this.reverseDeltaX();
     } 
     // if no wall hit
